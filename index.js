49b698759f8ae04714a8252f89bff4a82b692bd2
Pablo <BenzoFuryWolf pzarov0@gmail.com>
2025-11-13 14:09:33 +0300

    angular1

--- index.js
+++ index.js
@@ -1,1 +1,99 @@
 console.log(1);
+
+//practic 1
+//1
+var string = 'Privet';
+var nomer = 69;
+var shtoto = false;
+
+//2
+var variable = 5;
+variable = "текст";
+variable = true;
+var empty = null;
+function show() {
+    console.log("...");
+}
+show();
+
+//practic 2
+var count = 0;
+console.log(count);
+count = 99;
+console.log(count);
+
+//practic 3
+//1
+var a = 25;
+var b = 4;
+var sum = a + b;
+if (sum % 2 === 0) {
+    console.log("Число " + sum + " чётное");
+}
+if (sum % 2 != 0) {
+    console.log("Число " + sum + " нечётное");
+}
+
+//2
+var sc = 0;
+sc += 1;
+sc += 1;
+sc += 1;
+sc -= 1;
+sc -= 1;
+console.log(sc);
+
+//3
+var wallet = 1000;
+wallet += 200;
+wallet *= 0.7;
+wallet /= 2;
+console.log(wallet);
+
+//practic 4
+//1
+var speed = 80;
+var isRoadClear = false;
+if (speed <= 60 || !isRoadClear) {
+    console.log('Можно ехать');
+}
+if (speed >= 80 && isRoadClear) {
+    console.log('Опасно');
+}
+
+//2
+var hk = true;
+var knowsPassword = false;
+if (hk && knowsPassword) {
+    console.log("Полный доступ");
+}
+if ((hk && !knowsPassword) || (!hk && knowsPassword)) {
+    console.log("Ограниченный доступ");
+}
+if (!hk && !knowsPassword) {
+    console.log("Доступ запрещён");
+}
+
+//practic 5
+//1
+var sum1 = 0;
+for (var i = 1; i <= 10; i++) {
+    sum1 += i;
+}
+console.log(sum1);
+
+//2
+var A = 5;
+var res = 1;
+for (var i = 1; i <= 10; i++) {
+    res = A * i;
+    console.log(A + "×" + i + "=" + res);
+}
+
+//3
+var n = 6;
+var fac = 1;
+for (var i = 1; i <= n; i++) {
+    fac *= i;
+}
+console.log(fac);
--- index.ts
+++ index.ts
@@ -1,1 +1,99 @@
 console.log(1)
+
+//practic 1
+//1
+let string: string ='Privet';
+let nomer: number=69; 
+let shtoto: boolean=false;
+
+//2
+let variable: any = 5;
+variable = "текст";
+variable = true;
+let empty: null = null;
+function show(): void{
+    console.log("...");
+}
+show();
+
+//practic 2
+let count: number=0;
+console.log(count);
+count=99;
+console.log(count);
+
+//practic 3
+//1
+let a: number = 25;
+let b: number = 4;
+let sum: number = a + b;
+if (sum%2===0) {
+    console.log("Число " + sum + " чётное");
+}
+if (sum%2!=0) {
+    console.log("Число " + sum + " нечётное");
+}
+
+//2
+let sc: number=0;
+sc+=1;
+sc+=1;
+sc+=1;
+sc-=1;
+sc-=1;
+console.log(sc);
+
+//3
+let wallet: number=1000;
+wallet+=200;
+wallet*=0.7;
+wallet/=2;
+console.log(wallet);
+
+//practic 4
+//1
+const speed: number=80;
+const isRoadClear: boolean=false;
+if (speed<=60||!isRoadClear){
+console.log('Можно ехать');
+}
+if (speed>=80 && isRoadClear) {
+console.log('Опасно');
+}
+
+//2
+let hk: boolean = true;
+let knowsPassword: boolean = false;
+if (hk&&knowsPassword) {
+    console.log("Полный доступ");
+}
+if ((hk&&!knowsPassword)||(!hk&&knowsPassword)){
+    console.log("Ограниченный доступ");
+}
+if (!hk&&!knowsPassword){
+    console.log("Доступ запрещён");
+}
+
+//practic 5
+//1
+let sum1=0;
+for (let i=1;i<=10;i++){
+    sum1+=i;
+}
+console.log(sum1);
+
+//2
+let A=5;
+let res=1;
+for (let i=1;i<=10;i++){
+    res=A*i;
+    console.log(A+"×"+i+"="+res);
+}
+
+//3
+let n=6;
+let fac=1;
+for (let i=1;i<=n;i++){
+    fac*=i;
+}
+console.log(fac);
